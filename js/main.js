c3f5829e76652e7e6bff81c447913dd81a02c2e5
hophiphip <nighop@yandex.ru>
2021-08-18 23:41:41 +0400

    Added script

---
+++ js/main.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', () => {
+    const camera = document.querySelector('[camera]');
+    const marker = document.querySelector('a-marker');
+    let check;
+    let isFound;
+
+    marker.addEventListener('markerFound', (e) => {
+        isFound = true;
+        // Log marker position in intervals
+        let cameraPosition = camera.object3D.position;
+        let markerPosition = marker.object3D.position;
+        let distance = cameraPosition.distanceTo(markerPosition)
+
+        check = setInterval(() => {
+            cameraPosition = camera.object3D.position;
+            markerPosition = marker.object3D.position;
+            distance = cameraPosition.distanceTo(markerPosition)
+
+            console.log("Distance from marker:", marker.id, " is: ", distance);
+        }, 3000);
+    });
+
+    marker.addEventListener('markerLost', (e) => {
+        isFound = false;
+        clearInterval(check);
+    })
+});
